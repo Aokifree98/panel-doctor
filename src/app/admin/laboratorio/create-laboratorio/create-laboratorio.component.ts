830ec7d20313af930c19697fdeabb5fd30d11ed4
kayserjhamillex <anibaljhamil@gmail.com>
2022-03-08 11:44:46 -0500

    cambios en laboratorio

--- src/app/admin/laboratorio/create-laboratorio/create-laboratorio.component.ts
+++ src/app/admin/laboratorio/create-laboratorio/create-laboratorio.component.ts
@@ -86,10 +86,6 @@ export class CreateLaboratorioComponent implements OnInit {
     this.tipoService.saveTipo(this.tipo).subscribe(
       res => {
         this.tipo1 = res;
-        console.log(res);
-        this.toastr.info('se creo nuevo tipo');
-        this.bandera = 'elegido';
-        this.laboratorio.TipoId = this.tipo1.id;
       }, err => {
         this.toastr.error('Error Api');
         this.bandera = 'elegido';
--- src/app/admin/laboratorio/list-laboratorio/list-laboratorio.component.ts
+++ src/app/admin/laboratorio/list-laboratorio/list-laboratorio.component.ts
@@ -1,6 +1,8 @@
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Tipo } from 'src/app/models/tipo';
 import { Component, OnInit } from '@angular/core';
+import { TipoService } from 'src/app/services/tipo.service';
 import { LaboratorioService } from 'src/app/services/laboratorio.service';
 
 @Component({
@@ -10,22 +12,56 @@ import { LaboratorioService } from 'src/app/services/laboratorio.service';
 })
 export class ListLaboratorioComponent implements OnInit {
   laboratorios: any = [];
+  tipos: any = [];
+  bandera = false;
+  tipo: Tipo = {
+    id: 0,
+    Name: ''
+  };
+  mensaje;
+  codigotipo;
   constructor(
     private router: Router,
     private toastr: ToastrService,
+    private tipoService: TipoService,
     private laboratorioService: LaboratorioService
   ) { }
 
   ngOnInit(): void {
-    this.getlaboratorios();
+    // this.getlaboratorios();
+    this.gettipos();
   }
-  getlaboratorios() {
-    this.laboratorioService.getLaboratorios().subscribe(
+  gettipos() {
+    this.tipoService.getTipos().subscribe(
+      res => {
+        this.tipos = res;
+      }, err => {
+        this.toastr.error('Error Api tipo');
+      }
+    );
+  }
+  ver(codigo) {
+    const parametro = codigo;
+    this.laboratorioService.getLaboratorioTipo(parametro).subscribe(
       res => {
         this.laboratorios = res;
-        console.log(res);
       },
       err => console.error(err)
+    );
+  }
+  updateTipo() {
+    // llamando a laboratorio de creacion que esta enlazada con el api
+    this.tipoService.updateTipo(this.codigotipo, this.tipo).subscribe(
+      res => {
+        console.log(res);
+        this.mensaje = res;
+        this.toastr.success('Tipo actualizado');
+        this.bandera = false;
+      },
+      err => {
+        console.error(err);
+        this.toastr.error('no se pudo actualizar tipo');
+      }
     );
   }
   // tslint:disable-next-line: typedef
@@ -38,11 +74,22 @@ export class ListLaboratorioComponent implements OnInit {
       ]
     );
   }
+  edit(codigo) {
+    this.bandera = true;
+    this.codigotipo = codigo;
+    this.tipoService.getTipo(codigo).subscribe(
+      res => {
+        this.tipo = res;
+      }, err => {
+        this.toastr.error('Error api tipo get');
+      }
+    );
+  }
   // tslint:disable-next-line: typedef
   editar(codigo) {
     console.log(codigo);
     const codigoaeditar = codigo;
-    this.toastr.info('Editar laboratorio');
+    this.toastr.info('Editar Tipo');
     this.router.navigate(
       [
         'admin',
@@ -51,6 +98,21 @@ export class ListLaboratorioComponent implements OnInit {
         codigoaeditar
       ]
     );
+  }
+  delete(codigo) {
+    this.tipoService.deleteTipo(codigo).subscribe(
+      res => {
+        this.mensaje = res;
+        this.router.navigate(
+          [
+            'admin',
+            'laboratorio',
+            'list'
+          ]
+        );
+      }
+    );
+    window.location.reload();
   }
 
 }
