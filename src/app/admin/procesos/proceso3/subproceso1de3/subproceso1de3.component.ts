ce7f5279508bb8c81d390c5eb1a60b7ca914f434
kayserjhamillex <anibaljhamil@gmail.com>
2022-03-23 23:02:26 -0500

    imagen para el historial del dolor

--- src/app/admin/procesos/proceso3/subproceso1de3/subproceso1de3.component.ts
+++ src/app/admin/procesos/proceso3/subproceso1de3/subproceso1de3.component.ts
@@ -1,4 +1,9 @@
+import { ToastrService } from 'ngx-toastr';
+import { Cita } from 'src/app/models/Cita';
 import { Component, OnInit } from '@angular/core';
+import { ListCita } from 'src/app/models/listcita';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CitaService } from 'src/app/services/cita.service';
 
 @Component({
   selector: 'app-subproceso1de3',
@@ -6,10 +11,134 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subproceso1de3.component.css']
 })
 export class Subproceso1de3Component implements OnInit {
+  reserva: ListCita = {
+    id: 0,
+    Appointment: new Date(),
+    Pay: '',
+    Type: '',
+    Condition: '',
+    Referred: '',
+    Companion: '',
+    Relationship: '',
+    BloodPressure: '',
+    HeartRate: '',
+    BreathingFrequency: '',
+    Temperature: '',
+    Saturation: '',
+    SickTime: '',
+    CurrentEpisode: '',
+    StartWay: '',
+    SignsandSymptoms: '',
+    DescriptionProblem: '',
+    SurgicalHistory: '',
+    MedicalHistory: '',
+    AllergicHistory: '',
+    PhysicalExam: '',
+    Diagnosis: '',
+    LaboratoryExam: '',
+    AdminId: 0,
+    ClienteId: 0,
+    HorarioId: 0
+  };
+  frontbody = {
+
+  };
+  endbody = {
+
+  };
+  reservadetail: Cita = {
+    id: 0,
+    Appointment: new Date(),
+    Pay: '',
+    Type: '',
+    Condition: '',
+    Referred: '',
+    Companion: '',
+    Relationship: '',
+    BloodPressure: '',
+    HeartRate: '',
+    BreathingFrequency: '',
+    Temperature: '',
+    Saturation: '',
+    SickTime: '',
+    CurrentEpisode: '',
+    StartWay: '',
+    SignsandSymptoms: '',
+    DescriptionProblem: '',
+    SurgicalHistory: '',
+    MedicalHistory: '',
+    AllergicHistory: '',
+    PhysicalExam: '',
+    Diagnosis: '',
+    LaboratoryExam: '',
+    AdminId: 0,
+    ClienteId: 0,
+    HorarioId: 0,
+    admin: {
+      id: 0,
+      Name: '',
+      LastName: '',
+      Phone: '',
+      Email: '',
+      Photo: ''
+    },
+    cliente: {
+      id: 0,
+      Name: '',
+      LastName: '',
+      BirthDate: new Date(),
+      Job: '',
+      Direction: '',
+      Phone: '',
+      Gender: '',
+      CivilStatus: '',
+      DocumentNumber: '',
+      Email: '',
+      Photo: ''
+    },
+    horario: {
+      id: 0,
+      Day: '',
+      Cupo: 0,
+      EspecialistaId: 0,
+      HoraId: 0,
+      especialista: {
+        id: 0,
+        Turn: '',
+        EspecialidadId: 0,
+        DoctorId: 0,
+        especialidad: {
+          id: 0,
+          Name: '',
+          Price: 0
+        },
+        doctor: {
+          id: 0,
+          Name: '',
+          LastName: '',
+          Email: '',
+        },
+      },
+      hora: {
+        id: 0,
+        Turn: '',
+        Interval: '',
+        Start: '',
+        End: ''
+      }
+    }
+  };
+  ticket: any = this.reservadetail;
+  elcodigo;
+  mensaje;
   bandera = false;
   bannderaatras = false;
+  codigocita = 1;
   constructor(
-
+    private router: Router,
+    private toastr: ToastrService,
+    private reservaService: CitaService,
+    private activatedRoute: ActivatedRoute,
   ) { }
   adelante(events) {
     this.bandera = !this.bandera;
@@ -40,6 +169,49 @@ export class Subproceso1de3Component implements OnInit {
     }
   }
   ngOnInit(): void {
+    const parametro = this.codigocita.toString();
+    // const params = this.activatedRoute.snapshot.params;
+    if (parametro !== '0') {
+      this.reservaService.getCita(parametro).subscribe(
+        res => {
+          this.reserva = res;
+          this.ticket = res;
+          const codigo = this.ticket.id;
+          this.elcodigo = codigo;
+          this.toastr.success('su boleta');
+          // const estado = this.reserva.Type;
+          // if (estado === 'normal') {
+          //   this.router.navigate(
+          //     [
+          //       'admin',
+          //       'home'
+          //     ]
+          //   );
+          // }
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  historial() {
+    this.reserva.Condition = 'atendido';
+    this.reservaService.updateCita(this.elcodigo, this.reserva).subscribe(
+      actualizacion => {
+        this.mensaje = actualizacion;
+        this.toastr.info('Datos Registrados');
+        this.router.navigate(
+          [
+            'admin',
+            'home'
+          ]
+        );
+      }, err => {
+        this.toastr.error('Error Crear cita de historial');
+      }
+    );
   }
 
 }
